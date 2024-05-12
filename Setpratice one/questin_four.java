c20d2a603e3a18b9cd6f2157466c788affe31124
imabuzerkhan <abuzerk41@gmail.com>
2024-05-12 21:53:43 +0545

    new commit

---
+++ Setpratice one/questin_four.java
@@ -0,0 +1,14 @@
+public class questin_four{
+  public static void main(String args[]) {
+int a = 12;
+float b = 12.4f;
+byte c = 24;
+short d = 23;
+long e = 123;
+double f = 23.567;
+
+ double result = (f*e)+ (d*c)+(b*a); 
+ System.out.println(result);
+
+  }
+}
