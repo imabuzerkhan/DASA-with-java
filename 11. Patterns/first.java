88c97b3eeefcc59f4d1d5f2ecc7147897bdd728a
imabuzerkhan <abuzerk41@gmail.com>
2024-05-21 21:53:44 +0545

    first pattern

---
+++ 11. Patterns/first.java
@@ -0,0 +1,17 @@
+// ! print these patern 
+// * 
+// **
+// ***
+// ****
+
+public class first{
+  public static void main(String args[]){
+    for(int line = 1; line<=4; line++){
+      for(int j = 1; j<=line; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+  }
+}
