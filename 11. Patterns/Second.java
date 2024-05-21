d4e1b0fb6c22806fe810f10b85f4294d01b1ca9c
imabuzerkhan <abuzerk41@gmail.com>
2024-05-21 22:10:43 +0545

    another commit

---
+++ 11. Patterns/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+  public static void main(String args[]) {
+    for (int line = 4; line >= 1; line--) {
+      for (int j = 1; j <= line; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
+
