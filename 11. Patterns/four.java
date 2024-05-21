fad98deea9cb7d9416f5a301d0e21a2428a05ad0
imabuzerkhan <abuzerk41@gmail.com>
2024-05-21 22:30:28 +0545

    new

--- 11. Patterns/four.java
+++ 11. Patterns/four.java
@@ -2,7 +2,7 @@ public class four {
   public static void main(String[] args) {
     char ch = 'a';
     int n = 4;
-    for(int i=1; i<=7; i++ ){
+    for(int i=1; i<=n; i++ ){
       for(int j=1; j<=i; j++){
         System.out.print(ch);
         ch++;
