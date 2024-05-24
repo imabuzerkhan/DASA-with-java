6720fec8c9f99181497bea0b61e707e86782fc1a
imabuzerkhan <abuzerk41@gmail.com>
2024-05-24 21:35:31 +0545

    new commit

---
+++ 12. function/three.java
@@ -0,0 +1,14 @@
+public class three {
+  public static void Swap(int a ){
+    a =2;
+    
+   
+
+  }
+  public static void main(String[] args) {
+    int a =5;
+    Swap(a);
+    System.out.println(a);
+
+  }
+}
