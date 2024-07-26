cbdac5c686fd57e0ad06c8b9d4a02bd095447148
imabuzerkhan <abuzerk41@gmail.com>
2024-07-26 17:46:21 +0545

    first commit after a long time

---
+++ 13.function revision/first.java
@@ -0,0 +1,13 @@
+public class first{
+
+public static void Helloworld(){
+  System.out.println("hellow world");
+}
+
+  public static void main(String args[]){
+Helloworld();
+Helloworld();
+Helloworld();
+Helloworld();
+  }
+}
