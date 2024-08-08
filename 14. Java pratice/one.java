eeb677eefb923ef88130dbaf94a97a3e7e2740a4
imabuzerkhan <abuzerk41@gmail.com>
2024-08-08 21:11:12 +0545

    two .java

---
+++ 14. Java pratice/one.java
@@ -0,0 +1,9 @@
+//**  */ 1. Write a Java program to print 'Hello' on screen and your name on a separate line.
+
+
+public class one{
+  public static void main (String args[]){
+    System.out.println("Hello");
+    System.out.println("Abuzer khan");
+  }
+}
---
+++ 14. Java pratice/two.java
@@ -0,0 +1,12 @@
+// 2. Write a Java program to print the sum of two numbers.
+// Test Data:
+// 74 + 36
+
+public class two {
+  public static void main(String args[]){
+    int a = 74 ;
+    int b = 36;
+    int sum = a+b ; 
+    System.out.println(sum);
+  }
+}
