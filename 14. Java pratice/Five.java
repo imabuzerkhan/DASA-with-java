b1459427b552fef67c7193a442c9ff6ad150b875
imabuzerkhan <abuzerk41@gmail.com>
2024-08-08 21:24:50 +0545

    question 5

---
+++ 14. Java pratice/Five.java
@@ -0,0 +1,15 @@
+// 5. Write a Java program that takes two numbers as input and displays the product of two numbers.
+
+
+import java.util.*;
+
+
+public class Five {
+  public static void main ( String args[]){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int pro = a * b ;
+System.out.println(pro);
+  }
+}
