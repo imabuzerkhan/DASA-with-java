d907cda151807be8e712233f8d44d8f275dc2a8d
imabuzerkhan <abuzerk41@gmail.com>
2024-08-08 21:14:12 +0545

    divide

---
+++ 14. Java pratice/Three.java
@@ -0,0 +1,12 @@
+// Write a Java program to divide two numbers and print them on the screen.
+// Test Data :
+// 50/3
+
+public class Three {
+  public static void main(String args[]){
+    int a  = 50;
+    int b = 3;
+    int divide = a/b;
+    System.out.println(divide);
+  }
+}
