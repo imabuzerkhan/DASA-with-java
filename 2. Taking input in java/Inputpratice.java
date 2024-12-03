16f35e86a592392609e83059062624ea055f30e2
imabuzerkhan <abuzerk41@gmail.com>
2024-12-03 19:22:59 +0545

    some pratices

---
+++ 2. Taking input in java/Inputpratice.java
@@ -0,0 +1,43 @@
+/* multi line comment 
+ * how to take input in java lets pratice it 
+ */
+
+//?  firstly import the package
+/* 
+import java.util.*;
+
+public class Inputpratice {
+  public static void main (String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int sum = a+b ;
+    System.out.println(sum);
+  }
+}
+
+*/
+
+// import java.util.*;
+// public class Inputpratice{
+//   public static void main (String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int a  = sc.nextInt();
+//     int c = sc.nextInt();
+//     int product = a * c ;
+//     System.out.println("The product of the sum is : "  +  product) ;
+//   }
+// }
+
+// * Area of Squre
+
+import java.util.*;
+public class Inputpratice{
+  public static void main (String args []){
+Scanner sc = new Scanner(System.in);
+float radius = sc.nextFloat(); 
+float area = 3.17f * radius * radius ;
+System.out.println("the area of a radius is :" + area ) ;
+
+}
+}
