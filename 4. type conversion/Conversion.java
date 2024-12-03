36dc839f9fcc4de820c853eb9e3ae41713750855
imabuzerkhan <abuzerk41@gmail.com>
2024-12-03 20:32:51 +0545

    new

---
+++ 4. type conversion/Conversion.java
@@ -0,0 +1,30 @@
+public class Conversion {
+  public static void main (String args []){
+    // int a =4 ;
+    // long b =a ;
+    // System.out.println("the value of B is :" + b );
+
+
+
+    // possible lossy conversion from long to int
+// long a = 56;
+// int b = a ;
+// System.out.println(b);
+
+
+// integer value will store in float but float value will not store in int becaue its lossy conversion
+
+// int a = 23 ;
+// float b = a ;
+// System.out.println(b);
+
+
+// float value will not store in int becaue its lossy conversion
+
+// float a = 23.5f;
+// int b =a ;
+// System.out.println(b);
+
+
+  }
+}
