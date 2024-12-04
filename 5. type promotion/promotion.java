d043dddea303c4eb26e320e528819c1374f20ef3
imabuzerkhan <abuzerk41@gmail.com>
2024-12-04 20:39:12 +0545

    enoug for today

---
+++ 5. type promotion/promotion.java
@@ -0,0 +1,26 @@
+// public class promotion {
+//   public static void main (String args[]){
+//     char a = 'a';
+//     char b = 'c' ; 
+//     //* */ char c = a + c ; 
+//     System.out.println(a+b) ;
+//     // ! its throw an erro because type promtion is work here
+//   }
+// }
+
+
+// * second rule of type promotion 
+public class promotion {
+  public static void main (String args[]){
+    int a = 23;
+    int $ = 34 ;  // its not throw a error its working
+    float b = 45;
+    long c = 45 ;
+    double  d = 56 ;
+    double total = a + b + c + d + $ ;
+    System.out.println(total);
+
+
+  }
+}
+
