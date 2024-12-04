de5de7ee97404f54ee5d83288f7fd53f1ee7ec91
imabuzerkhan <abuzerk41@gmail.com>
2024-12-04 19:29:13 +0545

    type casting

---
+++ 3. Type casting/typecasting.java
@@ -0,0 +1,7 @@
+public class typecasting{
+  public static void main (String args[]){
+    float a = 32.56f ;
+    int b = (int)a ;
+    System.out.println(b);
+  }
+}
---
+++ 3. Type casting/typecasting2.java
@@ -0,0 +1,7 @@
+public class typecasting2 {
+  public static void main(String[] args) {
+    double price = 4678;
+    int newprice = (int)price ;
+    System.out.println(newprice);
+  }
+}
