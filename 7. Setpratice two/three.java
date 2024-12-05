22a58ef24a2732f1c18e71c150b007a08f4c5c14
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 21:48:31 +0545

    prTICE QUESTION

--- 7. Setpratice two/three.java
+++ 7. Setpratice two/three.java
@@ -2,9 +2,10 @@ public class three {
   public static void main(String[] args){
     int x, y, z;
     x = y = z = 2;
-    x += y;
-    y -= z;
-    z /= (x + y);
+    x += y; // 4
+    y -= z; //0
+    z /= (x + y); //0 z becomes 2 / 4 = 0 (integer division, so the result is truncated to 0).
+
     System.out.println(x + " " + y + " " + z);
     }
     }
