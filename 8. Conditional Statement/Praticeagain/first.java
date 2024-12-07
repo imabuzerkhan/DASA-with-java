79131f4eea35e56935363a054f30af360cd99fc7
imabuzerkhan <abuzerk41@gmail.com>
2024-12-07 18:33:15 +0545

    if else

---
+++ 8. Conditional Statement/Praticeagain/first.java
@@ -0,0 +1,35 @@
+/* check it eligible for vote or not
+ public class first {
+  public static void main(String args[]){
+    int age = 18 ;
+    if(age>=18){
+      System.out.println("it eligible for vote");
+    }
+    else{
+      System.out.println("it is not eligible for vote");
+    }
+  }
+}
+
+ */
+
+// ! print the largest no of 2
+
+import java.util.Scanner;
+
+public class first{
+  public static void main (String args[]) {
+  Scanner sc = new Scanner(System.in);
+  int num = sc.nextInt();
+int num2 = sc.nextInt();{
+  if(num >= num2 ) {
+    System.out.println("num is the largest number") ;
+  }
+  else{
+    System.out.println("num2 is the largest number") ;
+  }
+    
+  }
+}
+
+}
