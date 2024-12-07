5903c2dda7fbc5dc8389e8fab80e7ae78cbf4b88
imabuzerkhan <abuzerk41@gmail.com>
2024-12-07 18:39:26 +0545

    if else completed

--- 8. Conditional Statement/Praticeagain/first.java
+++ 8. Conditional Statement/Praticeagain/first.java
@@ -15,21 +15,36 @@
 
 // ! print the largest no of 2
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class first{
-  public static void main (String args[]) {
-  Scanner sc = new Scanner(System.in);
-  int num = sc.nextInt();
-int num2 = sc.nextInt();{
-  if(num >= num2 ) {
-    System.out.println("num is the largest number") ;
-  }
-  else{
-    System.out.println("num2 is the largest number") ;
-  }
+// public class first{
+//   public static void main (String args[]) {
+//   Scanner sc = new Scanner(System.in);
+//   int num = sc.nextInt();
+// int num2 = sc.nextInt();{
+//   if(num >= num2 ) {
+//     System.out.println("num is the largest number") ;
+//   }
+//   else{
+//     System.out.println("num2 is the largest number") ;
+//   }
     
-  }
-}
+//   }
+// }
 
+// }
+
+
+// ! odd and even
+import java.util.* ;
+public class first{
+  public static void main (String args[]){
+    Scanner sc =new Scanner(System.in) ;
+    int num = sc.nextInt();
+    if(num % 2 == 0){
+      System.out.println("it is a even number");
+    }else{
+      System.out.println("it is odd number");
+    }
+  }
 }
