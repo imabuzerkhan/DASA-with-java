c067b8c93470944eb83822f12b05ebbf22eefe39
imabuzerkhan <abuzerk41@gmail.com>
2024-12-07 19:38:45 +0545

    ternary

---
+++ 8. Conditional Statement/Praticeagain/Ternary.java
@@ -0,0 +1,14 @@
+public class Ternary {
+  public static void main(String[] args) {
+    // int num = 24;
+    // int num2 = 34;
+
+    // String result = (num>num2) ? "num is grater" : "num2 is grater" ;
+    // System.out.println(result);
+
+
+int Result = (24<34) ? 24 : 34 ;
+System.out.println(Result);
+
+  }
+}
