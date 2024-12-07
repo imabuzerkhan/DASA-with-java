16e1e373702000e9834e6f64bcec823581aafe5e
imabuzerkhan <abuzerk41@gmail.com>
2024-12-07 19:49:53 +0545

    Switch case

---
+++ 8. Conditional Statement/Praticeagain/switch1.java
@@ -0,0 +1,21 @@
+public class switch1 {
+  public static void main(String[] args) {
+    int number = 4;
+
+    switch (number) {
+      case 1: System.out.println("it is first case");;
+
+      break ;
+        
+        case 2 : System.out.println("second case");
+        break ;
+        case 3 : System.out.println("third case");
+        break ;
+        case 4 : System.out.println("Fourth case");
+        break ;
+      default: System.out.println("unkonow number");
+        break;
+    }
+  }
+  
+}
