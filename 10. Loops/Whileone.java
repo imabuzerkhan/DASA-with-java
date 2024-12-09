38929fef639b1948e9cbed079ed5fd8d3fe2e733
imabuzerkhan <abuzerk41@gmail.com>
2024-12-09 20:38:40 +0545

    practicing while loop

--- 10. Loops/Whileone.java
+++ 10. Loops/Whileone.java
@@ -1,10 +1,21 @@
-public class Whileone {
-  public static void main(String[] args) {
-    int a =0;
-    while (a<=100) {
-      System.out.print("Sobu i love you 😘😘");
-      a++;
-    }
+// public class Whileone {
+//   public static void main(String[] args) {
+//     int a =0;
+//     while (a<=100) {
+//       System.out.print("Sobu i love you 😘😘");
+//       a++;
+//     }
     
+//   }
+// }
+
+
+public class  Whileone{
+  public static void main (String args[]){
+    int i = 0 ;
+    while(i<=10){
+      System.out.print("hello world" + " ");
+      i++ ;
+    }
   }
 }
