52440470d76d06b102ea529c58ad98a4c2d933d4
imabuzerkhan <abuzerk41@gmail.com>
2024-12-09 21:21:18 +0545

    new coomit

--- 10. Loops/Whileone.java
+++ 10. Loops/Whileone.java
@@ -9,13 +9,48 @@
 //   }
 // }
 
+// ! pratice no 2
 
-public class  Whileone{
-  public static void main (String args[]){
-    int i = 0 ;
-    while(i<=10){
-      System.out.print("hello world" + " ");
-      i++ ;
+// public class  Whileone{
+//   public static void main (String args[]){
+//     int i = 0 ;
+//     while(i<=10){
+//       System.out.print("hello world" + " ");
+//       i++ ;
+//     }
+//   }
+// }
+
+
+// ! practice no 3
+// ? print 10 to 0 using while loop
+
+// public class Whileone{
+//   public static void main(String[] args) {
+//     int count = 10;
+//     while (count>=0) {
+//       System.out.print(count + " ");
+//       count-- ;
+      
+//     }
+//   }
+// }
+
+
+// ! practice no 4 
+// ? find the multiple of 2 
+
+public class Whileone {
+
+   public static void main(String[] args) {
+    int i = 2 ;
+   
+
+    while (i<=10) {
+      System.out.println(i);
+
+      i+=2;
+      
     }
-  }
+   }
 }
