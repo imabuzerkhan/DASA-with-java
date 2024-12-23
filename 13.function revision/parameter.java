c13fc5a349a6f1514c5400b4d0b156b9706865e4
imabuzerkhan <abuzerk41@gmail.com>
2024-12-23 22:24:33 +0545

    parameter with java

---
+++ 13.function revision/parameter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class parameter {
+  public static int CalculateSum(int num1 , int num2){
+    int sum = num1 + num2;
+    return sum ; 
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt() ;
+   int sum = CalculateSum(a , b);
+   System.out.println(sum);
+  }
+}
