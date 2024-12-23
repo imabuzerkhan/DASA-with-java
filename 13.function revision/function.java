14987b6df430c1e5df5448bea27ad028dd369815
imabuzerkhan <abuzerk41@gmail.com>
2024-12-23 22:19:33 +0545

    first push of the day

---
+++ 13.function revision/function.java
@@ -0,0 +1,16 @@
+public class function {
+
+public static void printname(){
+  System.out.println("mohammad abuzer khan");
+}
+
+  public static void main (String args[]){
+printname();
+printname();
+printname();
+printname();
+printname();
+printname();
+
+  }
+}
