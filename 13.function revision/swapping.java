a10593fe54e2833788290f8e7387c445676a3cc7
imabuzerkhan <abuzerk41@gmail.com>
2024-12-24 22:35:28 +0545

    normal swapping

---
+++ 13.function revision/swapping.java
@@ -0,0 +1,16 @@
+public class swapping {
+  public static void main (String args[]){
+    // swap // value exchange
+    int a = 10;
+    int b = 5 ;
+
+    // * swapping
+    int temp = a ;
+    a = b ;
+    b = temp ;
+
+    System.out.println("the value of a is :" + a );
+    System.out.println("the value of b is :" + b );
+
+  }
+}
