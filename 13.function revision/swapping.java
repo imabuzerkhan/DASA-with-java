aa816b659abc76ae1ca033b1069869b41a3d0365
imabuzerkhan <abuzerk41@gmail.com>
2024-12-24 22:48:10 +0545

    swapping

--- 13.function revision/swapping.java
+++ 13.function revision/swapping.java
@@ -1,16 +1,35 @@
 public class swapping {
+
+public static void swap(int a , int b){
+// * swapping
+ a = 20 ;
+
+
+int temp = a ;
+a = b ;
+b = temp ;
+
+// System.out.println("the value of a is :" + a );
+// System.out.println("the value of b is :" + b );
+
+
+
+// * note 
+// * jab tak function andar rahyega tab tak hii print statement wagera kaam karegga jasie bhar jayega waise main function kaam karen lage ga
+
+
+}
+
+
   public static void main (String args[]){
     // swap // value exchange
     int a = 10;
     int b = 5 ;
-
-    // * swapping
-    int temp = a ;
-    a = b ;
-    b = temp ;
+    swap(a, b);
 
     System.out.println("the value of a is :" + a );
     System.out.println("the value of b is :" + b );
+    
 
   }
 }
