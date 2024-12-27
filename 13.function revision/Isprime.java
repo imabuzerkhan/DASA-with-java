c1c68b35196803e0124433253f16884651ce94d2
imabuzerkhan <abuzerk41@gmail.com>
2024-12-27 19:15:13 +0545

    isprime

---
+++ 13.function revision/Isprime.java
@@ -0,0 +1,30 @@
+public class Isprime {
+
+public static boolean IsPrime(int n ){
+//   boolean  Isprime = true ;
+  if(n==2){
+    System.out.println("it is a prime number");
+    return true ;
+  
+  }
+  for(int i=2; i<=Math.sqrt(n); i++){
+    if(n%i==0){
+     System.out.println("it is not a prime number");
+  return false ;
+   
+    }else{
+      System.out.println("it is a prime number");
+      return true ;
+    }
+  }
+ return true ; 
+}
+
+
+
+  public static void main(String[] args) {
+   boolean value = IsPrime(2);
+   System.out.println(value);
+    
+  }
+}
