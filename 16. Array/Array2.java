3e6115400444ee4785779279d8c0fc42f2805631
imabuzerkhan <abuzerk41@gmail.com>
2024-12-30 20:31:52 +0545

    taking user from input

---
+++ 16. Array/Array2.java
@@ -0,0 +1,10 @@
+// another way to write a array 
+public class Array2 {
+  public static void main(String[] args) {
+    int marks[] = {10 , 20 ,30};
+    for(int i=0; i<marks.length; i++){
+      System.out.println(marks[i]);
+    }
+    // System.out.println(marks[0]);
+  }
+}
---
+++ 16. Array/char1.java
@@ -0,0 +1,8 @@
+public class char1 {
+  public static void main(String[] args) {
+    char alphabet[] = {'c', 'b'};
+    for(int i=0; i<alphabet.length; i++){
+System.out.print(alphabet[i] + " ");
+    }
+  }
+}
