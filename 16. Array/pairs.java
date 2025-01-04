c3a37d7f1036365e5f385781521c4768f23628c8
imabuzerkhan <abuzerk41@gmail.com>
2025-01-04 08:46:21 +0545

    Pairs in array

---
+++ 16. Array/pairs.java
@@ -0,0 +1,17 @@
+public class pairs {
+  public static void PairsArray(int num[]){
+    for(int i=0; i<num.length; i++){
+      int curr = num[i] ;
+      // ineer
+      for (int j = i+1; j<num.length; j++){
+        System.out.print("(" + curr + "," + num[j] + ")");
+        
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args) {
+    int num[] = {2,4,6,8};
+    PairsArray(num);
+  }
+}
