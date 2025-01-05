218897b6188540e9b505fc847bc5c99ff90e02f1
imabuzerkhan <abuzerk41@gmail.com>
2025-01-05 21:52:50 +0545

    traping water

---
+++ 16. Array/TrapingWater.java
@@ -0,0 +1,30 @@
+public class TrapingWater {
+  public static int TrapingRainWater(int height[]){
+    int n = height.length ;
+    // * left max boundary using auxilary verb
+    int leftmax[] = new int[n] ;
+    leftmax[0] = height[0];
+    for(int i=1; i<n; i++){
+      leftmax[i] = Math.max(height[i], leftmax[i-1] ) ;
+    }
+    //* */ calculate right max boundary using auxilary verb
+    int rightmax[] = new int[n];
+    rightmax[n-1] = height[n-1] ;
+    for(int i=2; i>=0; i--){
+      rightmax[i] = Math.max(height[i], rightmax[i+1]);
+    }
+    // trapped water
+    int trapedwater = 0;
+    // loop
+    for(int i=0; i<n; i++){
+      int waterlevel = Math.min(leftmax[i], rightmax[i]) ;
+      trapedwater += waterlevel - height[i] ; 
+    }
+    return trapedwater ;
+    
+  }
+  public static void main (String args[]){
+    int height[] = {4,2,0,6,3,2,5};
+    System.out.println(TrapingRainWater(height));
+  }
+}
