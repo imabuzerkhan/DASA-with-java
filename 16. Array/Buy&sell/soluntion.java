7f7f3a16a0354712a219435c6d0689a2f4a9bb73
imabuzerkhan <abuzerk41@gmail.com>
2025-01-06 18:47:03 +0545

    buy and sell

---
+++ 16. Array/Buy&sell/soluntion.java
@@ -0,0 +1,21 @@
+
+
+public class soluntion {
+  public static int Buy(int prices[]){
+    int n = prices.length ;
+    int BuyingPrices = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for(int i=0; i<n; i++){
+     if(BuyingPrices<prices[i]){
+      maxProfit = Math.max(maxProfit, prices[i]);
+     }else{
+      BuyingPrices = prices[i];
+     }
+    }
+    return maxProfit ;
+  }
+  public static void main(String[] args) {
+    int prices[] = {7,10,12,23,45,34} ;
+    System.out.println(Buy(prices));
+  }
+}
