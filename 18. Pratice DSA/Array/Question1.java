f0e925dc938da298e0d0cc65e0199523f13b7c72
imabuzerkhan <abuzerk41@gmail.com>
2025-01-11 09:08:25 +0545

    finding minimum number

---
+++ 18. Pratice DSA/Array/Question1.java
@@ -0,0 +1,19 @@
+/*
+ *Maximum and minimum of an array using minimum number of comparisons
+ */
+
+ public class Question1{
+  public static void minimumNum(int num[]){
+    int min = Integer.MAX_VALUE;
+    for(int i=0; i<num.length; i++){
+      if(min > num[i]){
+        min = num[i];
+      }
+    }
+    System.out.println("The minimum Number is " + min);
+  }
+  public static void main (String args[]){
+    int num[] = {1,2,3,4,8,4,2,8,6};
+    minimumNum(num);
+  }
+ }
