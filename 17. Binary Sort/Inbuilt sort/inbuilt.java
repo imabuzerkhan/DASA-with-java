6c9cb12f4e45a099ee94d11356be6b7e36f046e3
imabuzerkhan <abuzerk41@gmail.com>
2025-01-12 21:22:26 +0545

    Ibuilt sorting

--- 17. Binary Sort/Inbuilt sort/inbuilt.java
+++ 17. Binary Sort/Inbuilt sort/inbuilt.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class inbuilt {
   public static void main(String[] args) {
     int num[] = {2,3,5,1,4};
+    // Sorting the array
     Arrays.sort(num);
 
     for(int i=0; i<num.length; i++){
-      System.out.println(num);
+      System.out.println(num[i]);
     }
   }
 }
