c0e2d580c0d8dfc08e2328092f39979cc4c2031e
imabuzerkhan <abuzerk41@gmail.com>
2025-01-13 20:35:54 +0545

    writing code for print output

--- 19. 2D Array/first.java
+++ 19. 2D Array/first.java
@@ -19,5 +19,13 @@ public class first {
         matrix[i][j] = sc.nextInt();
       }
     }
+    // print the output now
+    for(int i=0; i<n; i++){
+      for(int j=0; j<m; j++){
+        System.out.print(matrix[i][j]);
+      }
+      System.out.println();
+    }
+
   }
 }
