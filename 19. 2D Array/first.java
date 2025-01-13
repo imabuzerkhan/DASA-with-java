bc5a7be7a29822a5c106a28ee02aefef46116db0
imabuzerkhan <abuzerk41@gmail.com>
2025-01-13 20:34:04 +0545

    writing input code for 2d array

--- 19. 2D Array/first.java
+++ 19. 2D Array/first.java
@@ -1,2 +1,23 @@
 // * 2D array
 // * creating 2d Array 
+import java.util.*;
+public class first {
+
+  public static void main(String[] args) {
+    // write 2d array
+    int matrix[][] = new int [3][3];
+    int n = matrix.length; int m= matrix[0].length;
+    // taking input
+    Scanner sc  = new Scanner(System.in);
+    // outer loop 
+    // loop for n and its mean row
+    for(int i=0; i<n; i++){
+      // inner loop
+      // loop for m and its mean columns
+      for(int j=0; j<m; j++){
+        // store i and j value in matrix and take input from user to show in output
+        matrix[i][j] = sc.nextInt();
+      }
+    }
+  }
+}
