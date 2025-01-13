d75c08a49770a5a88c8076349393fd711207d244
imabuzerkhan <abuzerk41@gmail.com>
2025-01-13 21:06:05 +0545

    call search function

--- 19. 2D Array/first.java
+++ 19. 2D Array/first.java
@@ -2,8 +2,23 @@
 // * creating 2d Array 
 import java.util.*;
 public class first {
-
+// making one search function to search any number or index
+public static boolean Search(int matrix[][], int key){
+  for(int i=0; i<matrix.length; i++){
+    for(int j=0; j<matrix[0].length; j++){
+      if(matrix[i][j]== key){
+        System.out.print("key found at( " + i + " , " + j + ")");
+        return true;
+      }
+      
+    }
+   
+  }
+  System.out.println("key not found");
+  return false;
+}
   public static void main(String[] args) {
+  
     // write 2d array
     int matrix[][] = new int [3][3];
     int n = matrix.length; int m = matrix[0].length;
@@ -28,6 +43,7 @@ public class first {
       // print new line
       System.out.println();
     }
-
+  // call search function
+  Search(matrix , 5);
   }
 }
