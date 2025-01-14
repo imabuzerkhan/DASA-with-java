f4f98727de9c5d0f682b827fbd932af2b500695e
imabuzerkhan <abuzerk41@gmail.com>
2025-01-14 21:10:15 +0545

    sprial matrix

--- 19. 2D Array/spiral.java
+++ 19. 2D Array/spiral.java
@@ -1,51 +1,49 @@
 public class spiral {
-  public static void PrintSprial(int matrix[][]){
-    int startRow= 0;
-    int Stratcol = 0 ;
-    int EndRow = matrix.length - 1 ;
-    int Endcol = matrix[0].length - 1; 
+  public static void PrintSprial(int matrix[][]) {
+      int startRow = 0;
+      int Stratcol = 0;
+      int EndRow = matrix.length - 1;
+      int Endcol = matrix[0].length - 1;
 
-while(startRow <= EndRow &&  Stratcol<= Endcol){
-  // top 
-  for(int j=Stratcol; j<Endcol; j++){
-    System.out.print(matrix[startRow][j]);
+      while (startRow <= EndRow && Stratcol <= Endcol) {
+          // Top boundary
+          for (int j = Stratcol; j <= Endcol; j++) {
+              System.out.print(matrix[startRow][j] + " ");
+          }
+          startRow++;
+
+          // Right boundary
+          for (int i = startRow; i <= EndRow; i++) {
+              System.out.print(matrix[i][Endcol] + " ");
+          }
+          Endcol--;
+
+          // Bottom boundary
+          if (startRow <= EndRow) { // Ensure there is a bottom row to print
+              for (int j = Endcol; j >= Stratcol; j--) {
+                  System.out.print(matrix[EndRow][j] + " ");
+              }
+              EndRow--;
+          }
+
+          // Left boundary
+          if (Stratcol <= Endcol) { // Ensure there is a left column to print
+              for (int i = EndRow; i >= startRow; i--) {
+                  System.out.print(matrix[i][Stratcol] + " ");
+              }
+              Stratcol++;
+          }
+      }
+      System.out.println();
   }
-  // right 
-  for (int i=startRow+1; i<EndRow; i++){
-    if(startRow == EndRow){
-      break;
-    }
-    System.out.println(matrix[i][Endcol]);
-  }
-  // botom
-  for (int j=Endcol-1; j<Stratcol; j--){
-    if(Stratcol == Endcol){
-      break;
-    }
-    System.out.println(matrix[EndRow][j]);
-// left
-for (int i=EndRow-1 ; i<startRow+1; i--){
-  System.out.println(matrix[i][Stratcol]);
 
-}
-Stratcol ++ ;
-startRow ++ ;
-EndRow -- ;
-Endcol -- ;
-  }
-  System.out.println();
-}
-
-
-
-    }
   public static void main(String[] args) {
-    int matrix[][] = {
-      {1,2,3,4},
-      {5,6,7,8},
-      {9,10,11,12},
-      {13,14,15,16}
-    };
-    PrintSprial(matrix) ; 
+      int matrix[][] = {
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 16}
+      };
+      PrintSprial(matrix);
   }
 }
