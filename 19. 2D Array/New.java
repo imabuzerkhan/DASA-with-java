ef2fc178bca5f5e506a1513b36178d1dd25745b5
imabuzerkhan <abuzerk41@gmail.com>
2025-01-16 19:20:36 +0545

    why we write matrix[0].length understand

---
+++ 19. 2D Array/New.java
@@ -0,0 +1,11 @@
+public class New {
+  public static void main(String[] args) {
+    int matrix[][] = {
+      {1,2,3,4},
+      {5,6,7,8},
+      {9,10,11,12}
+    } ;
+  System.out.println("rows = "  + matrix.length );
+  System.out.println("Col = "  + matrix[0].length );
+  }
+}
